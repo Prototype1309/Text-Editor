a04f715deca30e6c6d16fd97ebd9a2021e80c8dd
prototype1309 <simo.hourri@gmail.com>
2023-08-12 15:06:49 -0400

    adding logic for installing in the js folder

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,6 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  
   // console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
@@ -26,7 +27,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  
+
   console.log("Text has been retrieved from the database.");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
